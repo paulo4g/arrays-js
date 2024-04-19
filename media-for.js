90af3023ffb73f637127a9423d7da301560b446c
Paulo Henrique <heypaulim@gmail.com>
2024-04-19 01:34:05 -0300

    estudos sobre arrays em javascript

--- media-for.js
+++ media-for.js
@@ -3,7 +3,7 @@
 //Calcular a média entre as seguintes notas usando o for:
 // 10, 6.5, 8, 7.5
 
-const notas = [10, 6.5, 8, 7.5];
+const notas = [10, 6.5, 8, 1];
 
 let somaDasNotas = 0;
 
@@ -11,4 +11,9 @@ for (let i = 0; i < notas.length; i++) {
     somaDasNotas += notas[i]
 }
 const media = somaDasNotas/notas.length
-console.log(media);
+
+if(media >= 7) {
+    console.log(`A média das notas é ${media} e você foi aprovado!`);
+} else {
+    console.log(`Sinto muito, como sua média foi ${media}, você não foi aprovado`);	
+}
